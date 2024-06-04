304741b5a62cdba6cf190e59c9a3228df903f450
CyberMan010 <mohamahasoun60@gmail.com>
2024-06-04 09:24:16 +0300

    iz

---
+++ Website/assets/js/app.js
@@ -0,0 +1,95 @@
+// let name = prompt("Please enter your Name");
+//         var gender = prompt("Please enter your gender");
+
+//         if (gender == 'male')
+//         {
+//             alert("Welcome, MR " + name);
+
+//         }
+//         else if (gender == 'female')
+//         {
+//             alert("Welcome, MS " + name);
+
+//         } else {
+//             alert("Welcome, " + name);
+//         }
+
+// let order = prompt("do you want to order Donut , Coffee , Icecream or Bakery?");
+         
+        
+//          if (order == 'Coffee', 'Donut', 'Icecream', 'Bakery')
+//             {
+                
+//                 alert("your " + order + " is getting prepared");
+//             }
+            
+//          else {
+//                 alert("Thank you for visiting our factory");
+//             }
+// console.log(name,order);
+
+// Q1
+
+// console.log(-5*3);
+// console.log( "JavaScript" + 50);
+// console.log( 17 % 5 );
+// console.log( 5 % 17);
+// console.log( 5/10);
+// console.log( (4 === '4'));
+// console.log( (4 != 5) );
+// console.log( (7 <= 8));
+// console.log( "Hello" + 5);
+// let x = 3
+// console.log(  Math.ceil(x) - Math.floor(x) );
+// console.log(Math.pow(x,2));
+// var y = prompt("enter number");
+// alert(y);
+
+// Q2
+
+// let y = prompt("enter the first number")
+// let x = prompt("enter the second number")
+//     if(x >= y){
+//         console.log(x)
+    
+//     }
+//     else{
+//             console.log(y)
+//     }    
+
+// Q3
+
+// let max  = Math.max(8, 5);
+// console.log("the largest number is ", max);
+
+// let y = prompt("enter the evalu")
+// let x = prompt("enter the second evalu")
+
+// let sum = (parseInt(x,10) + parseInt(y,10))
+// console.log(sum)
+
+// Q6
+
+// let number = prompt("enter number to see ")
+// switch(number){
+//     case 1:
+//         console.log("1")
+//     case 2:
+//         console.log("2")
+//     case 3:
+//         console.log("3")
+//     case 4:
+//         console.log("4")
+//     case 5:
+//         console.log("5")
+//     case 6:
+//         console.log("6")
+//     case 7:
+//         console.log("7")
+//     case 8:
+//         console.log("8")
+//     case 9:
+//         console.log("9")
+//     default:
+//         console.log("PLEASE TRY AGAIN")
+// }
