fae15b7f3c2948f8143c90d56a1a549f48c0e1b7
CyberMan010 <mohamahasoun60@gmail.com>
2024-06-05 06:43:23 +0300

    a tas

--- Website/assets/js/app.js
+++ Website/assets/js/app.js
@@ -1,18 +1,121 @@
-// let name = prompt("Please enter your Name");
-//         var gender = prompt("Please enter your gender");
+// let name = prompt("enter your name")
+// let userGender;
 
-//         if (gender == 'male')
-//         {
-//             alert("Welcome, MR " + name);
+// while (userGender !== "male" && userGender !== "female") {
+//   userGender = prompt("Enter your gender:").toLowerCase();
+        
+// }
+// if(userGender == 'male')
+//     alert("Welcome Mr " + name);
+// else
+//  alert("Welcome Ms " + name)
 
-//         }
-//         else if (gender == 'female')
-//         {
-//             alert("Welcome, MS " + name);
+// Q7
 
-//         } else {
-//             alert("Welcome, " + name);
-//         }
+// for (let i = 0; i <= 5; i++) {
+//     alert(i);
+//   }
+
+// Q8
+
+// let message = " ";
+// for (let i = 0; i <= 5; i++) {
+//   message += i ; 
+// }
+
+// alert(message);
+
+// Q9
+
+// let message = " ";
+// for (let i = 0; i <= 5; i++) {
+//   message += i ; 
+// }
+
+// alert(message);
+
+// Q9
+
+// let message = " ";
+
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 3 === 0) {
+    
+//     message += i + " ";
+//   }
+// }
+// alert(message)
+
+// Q10
+
+// let number;
+
+// do {
+//   number = parseInt(prompt("Enter a number between 0 and 100 (inclusive):"));
+
+//   if (isNaN(number) || number < 0 || number > 100) {
+//     alert("Invalid number. Please enter a number between 0 and 100.");
+//   }
+// } while (isNaN(number) || number < 0 || number > 100);
+
+// alert("You entered: " + number);
+
+// Q11
+
+
+    let sum = 0;
+  
+    // Iterate from 0 to the entered number (inclusive) and add each number to the sum
+    for (let i = 0; i <= number; i++) {
+      sum += i;
+    }
+  
+    return sum;
+  
+  
+  const userNumber = parseInt(prompt("Enter an integer: "));
+  
+  // Check if the input is a valid number (not NaN)
+  if (!isNaN(userNumber)) {
+    const sum = calculateSum(userNumber);
+    alert("The sum of integers from 0 to " + userNumber + " is: " + sum);
+  } else {
+    alert("Invalid input. Please enter a valid integer.");
+  }
+
+
+// let name = prompt("enter your name")
+// let userGender;
+
+// while (userGender !== "male" && userGender !== "female") {
+//   userGender = prompt("Enter your gender:").toLowerCase();
+        
+// }
+// if(userGender == 'male')
+//     alert("Welcome Mr " + name);
+// else
+//  alert("Welcome Ms " + name)
+// console.log(userGender + name)
+
+// let userInfo = [];
+
+// function askQuestion(question) {
+//   const answer = prompt(question);
+//   return answer;
+// }
+
+// const userName = askQuestion("Enter your username: ");
+// const gender = userGender;
+// const orderType = askQuestion("What type of order do you want (e.g., pickup, delivery): ").toLowerCase();
+// const orderName = askQuestion("Enter the name of your order: ");
+
+// userInfo.push(userName, gender, orderType, orderName);
+
+// console.log("order details:");
+
+// for (let i = 0; i < userInfo.length; i++) {
+//   console.log(`${i + 1}. ${userInfo[i]}`);
+// }
 
 // let order = prompt("do you want to order Donut , Coffee , Icecream or Bakery?");
          
@@ -26,7 +129,9 @@
 //          else {
 //                 alert("Thank you for visiting our factory");
 //             }
-// console.log(name,order);
+// let details = [name, userGender, order, "Bakery"]
+
+// console.log(details);
 
 // Q1
 
@@ -93,3 +198,4 @@
 //     default:
 //         console.log("PLEASE TRY AGAIN")
 // }
+
